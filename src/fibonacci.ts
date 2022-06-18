d604e147e36d3ce3e484f43a4deff6e71848d13c
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:08:00 +0100

    handle null input

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,22 +1,23 @@
-export const computeFibonacciNumber = (position: number, isRecursive: boolean = false): number => {
+export const computeFibonacciNumber = (position: number , isRecursive: boolean = false): number => {
     if (isRecursive) return recursiveFibonacci(position);
+    let notNullPosition = position;
+    if (notNullPosition === null) {
+        notNullPosition = 1;   
+    }
     let i = 1;
     let j = 1;
-
-    if (position === 0) {
+    if (notNullPosition === 0) {
         return 0;
     }
-    if (position < 0) {
-        return computeNegativeFibonacci(position, isRecursive);
+    if (notNullPosition < 0) {
+        return computeNegativeFibonacci(notNullPosition, isRecursive);
     }
-
-    if (position <= 2) {
+    if (notNullPosition <= 2) {
         return 1;
     }
 
-
     let currentPosition = 2;
-    while (currentPosition < position) {
+    while (currentPosition < notNullPosition) {
         const temp = i;
         i = j;
         j += temp;
